a918131692ee635bf59bce44a60779ebf0d10642
Arthur Caumartin <artcaumart@gmail.com>
2024-01-25 15:18:32 +0100

    lzieurgoegrh

--- Assets/Scripts/SpriteSwaper.cs
+++ Assets/Scripts/SpriteSwaper.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SpriteSwaper : MonoBehaviour
 {
     public List<Sprite> _spriteList;
     private SpriteRenderer _spriteReder;
+    bool _trailDirection;
+
+    public Animation swipeAnim;
 
     void Start()
     {
@@ -13,6 +17,14 @@ public class SpriteSwaper : MonoBehaviour
 
     public void SwapSprite(int index)
     {
+        _trailDirection = !_trailDirection;
         _spriteReder.sprite = _spriteList[index];
+        if(swipeAnim)
+        {
+            if(_trailDirection)
+                swipeAnim.Play("SwipeLeft");
+            else
+                swipeAnim.Play("SwipeRight");
+        }
     }
 }
