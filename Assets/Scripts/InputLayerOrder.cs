53735d6b8fbcc1800c34c74e45517b7d1bdc60d8
D'alessandro Ethan <roger.paldi@yahoo.com>
2024-01-25 13:18:48 +0100

    NewSprite & ShakeInput

--- Assets/Scripts/InputLayerOrder.cs
+++ Assets/Scripts/InputLayerOrder.cs
@@ -1,20 +1,47 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using DG.Tweening;
 
 public class InputLayerOrder : MonoBehaviour
 {
-    public Color baseColor;
+    public SpriteRenderer spriteRenderer;
+    public Color notOnColor;
+    public Color onColor;
+    public Color failedColor;
     public Color targetColor;
+
+    private void Awake()
+    {
+        targetColor = notOnColor;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     public void changeLayerOrder(InputAction.CallbackContext context)
     {
-        if(context.performed)
+        if (context.performed)
         {
-            transform.GetComponent<SpriteRenderer>().sortingOrder = 51;
+            targetColor = onColor;
         }
         else
         {
-            transform.GetComponent<SpriteRenderer>().sortingOrder = 49;
+            targetColor = notOnColor;
         }
+    }
+
+    public void DefaultInput()
+    {
+        transform.DOShakePosition(.25f, .15f);
+        transform.DOScale(1.25f, .15f).SetLoops(2, LoopType.Yoyo);
+    }
+
+    public void FailedInput()
+    {
+        targetColor = failedColor;
+        transform.DOShakePosition(.25f, 1f, 100);
+    }
+
+    private void Update()
+    {
+        transform.GetComponent<SpriteRenderer>().color = Color.Lerp(spriteRenderer.color, targetColor, Time.deltaTime * 50); ;
     }
 }
